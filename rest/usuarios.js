3d72e9b4a0d48210f6a6cfa5682a73171e0947e4
leolopezv <leonardo.lopez@uees.edu.ec>
2024-08-02 15:10:15 -0500

    Implementacion de delete y update para usuarios

--- rest/usuarios.js
+++ rest/usuarios.js
@@ -3,16 +3,6 @@ var router = express.Router();
 const { Sequelize, Op} = require('sequelize');
 const Usuario = require('../models').usuario;
 
-router.get('/findAll/json', function(req, res, next) {  
-    Usuario.findAll({  
-        attributes: { exclude: ["createdAt","updatedAt"] },
-    })  
-    .then(usuarios => {  
-        res.json(usuarios);  
-    })  
-    .catch(error => res.status(400).send(error)) 
-});
-
 router.post('/save', function(req, res, next) {
     let { nombre, correo, celular, comentario } = req.body;
     Usuario.create({
@@ -28,5 +18,44 @@ router.post('/save', function(req, res, next) {
     })
     .catch(error => res.status(400).send(error));
   });
+
+  router.get('/findAll/json', function(req, res, next) {  
+    Usuario.findAll({  
+        attributes: { exclude: ["createdAt","updatedAt"] },
+    })  
+    .then(usuarios => {  
+        res.json(usuarios);  
+    })  
+    .catch(error => res.status(400).send(error)) 
+});
+
+router.put('/update/:id', function(req, res, next) {
+  let { nombre, correo, celular, comentario } = req.body;
+  let id = parseInt(req.params.id);
+  Usuario.update({
+    nombre: nombre,
+    correo: correo,
+    celular: celular,
+    comentario: comentario,
+    updatedAt: new Date()
+  }, {
+    where: { id: id } 
+  })
+  .then(respuesta => {
+    res.json(respuesta);
+  })
+  .catch(error => res.status(400).send(error));
+});
+
+router.delete('/delete/:id', function(req, res, next) {
+  let id = parseInt(req.params.id);
+  Usuario.destroy({
+    where: { id: id }
+  })
+  .then(respuesta => {
+    res.json(respuesta);
+  })
+  .catch(error => res.status(400).send(error));
+});
 
 module.exports = router;

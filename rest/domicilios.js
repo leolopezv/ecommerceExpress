f56206680f6b1abd6691c838aad1dacddab60801
leolopezv <leonardo.lopez@uees.edu.ec>
2024-08-09 21:34:21 -0500

    version estable 08/09/24

--- rest/domicilios.js
+++ rest/domicilios.js
@@ -19,4 +19,35 @@ router.get('/findAll/json', function(req, res, next) {
     .catch(error => res.status(400).send(error)) 
 });
 
+router.put('/update/:id', function(req, res, next) {
+  let { pais, provincia, ciudad, direccion, postal, usuario_id } = req.body;
+  let id = parseInt(req.params.id);
+  Domicilio.update({
+    pais: pais,
+    provincia: provincia,
+    ciudad: ciudad,
+    direccion: direccion,
+    postal: postal,
+    usuario_id: usuario_id,
+    updatedAt: new Date()
+  }, {
+    where: { id: id }
+  })
+  .then(respuesta => {
+    res.json(respuesta);
+  })
+  .catch(error => res.status(400).send(error));
+});
+
+router.delete('/delete/:id', function(req, res, next) {
+  let id = parseInt(req.params.id);
+  Domicilio.destroy({
+    where: { id: id }
+  })
+  .then(respuesta => {
+    res.json(respuesta);
+  })
+  .catch(error => res.status(400).send(error));
+});
+
 module.exports = router;

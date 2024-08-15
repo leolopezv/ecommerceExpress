1a5965f2553d7eda857cf78f4b2e93a07800db0b
gabriel30102002 <gabrielgue30@gmail.com>
2024-08-14 23:19:56 -0500

    agregacion

--- rest/fotos.js
+++ rest/fotos.js
@@ -14,4 +14,52 @@ router.get('/findAll/json', function(req, res, next) {
     .catch(error => res.status(400).send(error)) 
 });
 
+// FILTRO POR PRECIO DE MAYOR A MENOR Y DE MENOR A MAYOR
+router.get('/findAllByPrice/json', function(req, res, next) {
+    let order = req.query.order === 'desc' ? 'DESC' : 'ASC'; // Verifica si se debe ordenar de manera descendente, por defecto es ascendente
+    Foto.findAll({
+        attributes: { exclude: ["updatedAt"] },
+        order: [
+            ['precio', order] // Ordenar por el atributo "precio"
+        ]
+    })
+    .then(fotos => {
+        res.json(fotos);
+    })
+    .catch(error => res.status(400).send(error));
+});
+
+router.get('/findAllByPrice/view', function(req, res, next) {
+    let order = req.query.order === 'desc' ? 'DESC' : 'ASC'; // Verifica si se debe ordenar de manera descendente, por defecto es ascendente
+    Foto.findAll({ 
+        attributes: { exclude: ["updatedAt"] },
+        order: [
+            ['precio', order] // Ordenar por el atributo "precio"
+        ]
+    })
+    .then(fotos => {
+        // Cambiar el nombre de la vista si es necesario
+        res.render('productos', { title: 'Fotos', arrFotos: fotos });
+    })
+    .catch(error => res.status(400).send(error));
+});
+
+router.get('/findAllByDescripcion/json', function(req, res, next) {
+    let order = req.query.order === 'desc' ? 'DESC' : 'ASC'; // Por defecto, es ascendente
+    Foto.findAll({
+        attributes: { exclude: ["createdAt", "updatedAt"] },
+        order: [
+            ['descripcion', order] // Ordenar por el atributo "descripcion"
+        ]
+    })
+    .then(fotos => {
+        res.json(fotos);
+    })
+    .catch(error => res.status(400).send(error));
+});
+
+
 module.exports = router;
+
+
+
